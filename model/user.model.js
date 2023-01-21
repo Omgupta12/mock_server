64fa6934f7a264a27d64370e08e39c5a0d3aa959
Omgupta12 <rahulgupta1997242@gmail.com>
2023-01-21 12:19:33 +0530

    timestamp add

--- model/user.model.js
+++ model/user.model.js
@@ -4,8 +4,9 @@ const UserSchema = new Schema({
     username:{type:String,required:true},
     email:{type:String, unique: true,required:true},
     password:{type:String,required:true}
-})
-
+},
+{ timestamps: true }
+)
 const UserModel = model("user",UserSchema)
 
 module.exports = UserModel
