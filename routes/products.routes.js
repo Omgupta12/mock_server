20ed170c9d7034c5c4ffff7e074c46d23c512c11
Omgupta12 <rahulgupta1997242@gmail.com>
2023-03-19 17:54:33 +0530

    version 3

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -2,20 +2,22 @@ const express = require("express");
 const productsModel = require("../model/products.model");
 const app = express.Router();
 
-
 app.get("/", async (request, response) => {
   const { page, limit } = request.query;
 
   try {
     const total = await productsModel.find({}).count();
 
-    const data = await productsModel.find({}).skip((page - 1) * limit).limit(limit);
+    const data = await productsModel
+      .find({})
+      .skip((page - 1) * limit)
+      .limit(limit);
 
-    const totalPage = Math.floor(total / limit)
+    const totalPage = Math.floor(total / limit);
 
-    return response.status(200).json({ products: data, totalPages: totalPage });
+    return response.status(200).send(data);
   } catch (error) {
-    return response.status(500).json({ error: error.message });
+    return response.status(500).send({ error: error.message });
   }
 });
 
